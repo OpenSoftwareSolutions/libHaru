912f681bcd2b3b1f2ce49c995027e5e6b1ca5722
Jakub Petrík <petrik@inloop.eu>
2016-07-23 12:28:50 +0200

    Initial commit

---
+++ Sources/libHaru.h
@@ -0,0 +1,46 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for libHaru.
+FOUNDATION_EXPORT double libHaruVersionNumber;
+
+//! Project version string for libHaru.
+FOUNDATION_EXPORT const unsigned char libHaruVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libHaru/PublicHeader.h>
+
+#import <libHaru/hpdf.h>
+#import <libHaru/hpdf_3dmeasure.h>
+#import <libHaru/hpdf_annotation.h>
+#import <libHaru/hpdf_catalog.h>
+#import <libHaru/hpdf_conf.h>
+#import <libHaru/hpdf_config.h>
+#import <libHaru/hpdf_consts.h>
+#import <libHaru/hpdf_destination.h>
+#import <libHaru/hpdf_doc.h>
+#import <libHaru/hpdf_encoder.h>
+#import <libHaru/hpdf_encrypt.h>
+#import <libHaru/hpdf_encryptdict.h>
+#import <libHaru/hpdf_error.h>
+#import <libHaru/hpdf_exdata.h>
+#import <libHaru/hpdf_ext_gstate.h>
+#import <libHaru/hpdf_font.h>
+#import <libHaru/hpdf_fontdef.h>
+#import <libHaru/hpdf_gstate.h>
+#import <libHaru/hpdf_image.h>
+#import <libHaru/hpdf_info.h>
+#import <libHaru/hpdf_list.h>
+#import <libHaru/hpdf_mmgr.h>
+#import <libHaru/hpdf_namedict.h>
+#import <libHaru/hpdf_objects.h>
+#import <libHaru/hpdf_outline.h>
+#import <libHaru/hpdf_page_label.h>
+#import <libHaru/hpdf_pages.h>
+#import <libHaru/hpdf_pdfa.h>
+#import <libHaru/hpdf_streams.h>
+#import <libHaru/hpdf_types.h>
+#import <libHaru/hpdf_u3d.h>
+#import <libHaru/hpdf_utils.h>
+#import <libHaru/hpdf_version.h>
+#import <libHaru/t4.h>
+
+
